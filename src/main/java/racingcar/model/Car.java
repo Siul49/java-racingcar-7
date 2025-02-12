c71334a49766d8254e859759ce13c815d417ed16
Siul <kksu149@gmail.com>
2025-02-12 23:33:42 +0900

    Car.java 모델 생성

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,29 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car{
+    private int SpeedPerSecond;
+    private int score;
+    private String name;
+    private
+
+    Car(String carName){
+        SpeedPerSecond = Randoms.pickNumberInRange(0,9);
+        name = carName;
+        score = 0;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public int getSpeedPerSecond(){
+        return SpeedPerSecond;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
